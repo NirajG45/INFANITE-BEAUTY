f35743e890a2ba99bb48b9e7b86c260f218f6f3a
Niraj Kumar <nirajkumar9945111@gmail.com>
2025-07-08 23:21:01 +0530

    adding enhanced js codes and fixing bugs

--- script.js
+++ script.js
@@ -120,3 +120,13 @@ AOS.init({
   easing: 'ease-in-out',
   once: true,
 });
+
+// Initialize tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// Initialize popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {
+  trigger: 'focus'
+}));
